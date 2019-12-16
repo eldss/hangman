9336745bed79695747ace6a4445c8e2da9f28afa
Evan <EvanLDouglass@gmail.com>
2019-12-16 14:11:16 -0800

    Ensure bootstrap integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -6,6 +8,17 @@ function App() {
   return (
     <div className="App">
       <h1>Testing</h1>
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+      <footer>
+        <small>
+          Images provided by CC BY-SA 3.0,&nbsp;
+          <a href="CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=892208">
+            https://commons.wikimedia.org/w/index.php?curid=892208
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
